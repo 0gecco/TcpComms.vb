c96c2f97b8be0e555d68bc15c042c77292c578cc
nitro_gecko <cogware67@gmail.com>
2025-07-28 01:34:04 +0200

    Delete BufferPools.vb
    Cleaning up

--- BufferPools.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Module BufferPools
-
-    Public ReadOnly Small As New SimpleBufferPool(4096)    ' 4 KB
-    Public ReadOnly Medium As New SimpleBufferPool(65536)  ' 64 KB
-    Public ReadOnly Large As New SimpleBufferPool(1048576) ' 1 MB
-
-End Module
